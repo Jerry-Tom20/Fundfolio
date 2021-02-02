f182a2cec266342c629758e3579cf3daba583e57
Jeffrey Jaison <jeffreyjaison2000@gmail.com>
2021-02-02 15:45:52 +0530

    Gogkul 2

--- lib/doctype/exist.dart
+++ lib/doctype/exist.dart
@@ -10,26 +10,70 @@ class PatientList extends StatefulWidget {
 
 class _PatientListState extends State<PatientList> {
   final firestoreInstance = FirebaseFirestore.instance;
+  QuerySnapshot data;
+  bool isLoading = true;
   @override
   void initState() {
-    // TODO: implement initState
-    firestoreInstance.collection("patient").get().then((querySnapshot) {
-    querySnapshot.docs.forEach((result) {
-      print(result.data());
+    print('Hello');
+    
+    firestoreInstance.collection('patient').get().then((querysnapshot) {
+      print(querysnapshot.docs.length);
+     setState(() {
+       isLoading = !isLoading;
+       data=querysnapshot;
+     });
     });
-  });
+     
+    
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    if(isLoading){
+      return Scaffold(body:Center(child: Text('Loading'),));
+    }
+
+    else{
+      int len = data.docs.length;
+      return Scaffold(
       appBar: AppBar(title: Text('Existing Patient'),
       
       ),
 
-      body: Container(
-        child:Center(child: Text('Hello'))
-      ),
+      body: new ListView.builder
+  (
+    itemCount:len,
+    itemBuilder: (BuildContext ctxt, int index) {
+     return Padding(
+       padding: const EdgeInsets.fromLTRB(0,10,0,0),
+       child: Container(height: 200, 
+       width: MediaQuery.of(context).size.width, 
+       decoration: new BoxDecoration(
+         color:Colors.deepPurple[300]
+       ),
+       child: Padding(
+         padding: const EdgeInsets.all(8.0),
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             new Text(data.docs.toList()[index].data()['name'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Medical History '+data.docs.toList()[index].data()['Medical History'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Age '+data.docs.toList()[index].data()['age'].toString(),style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Date '+data.docs.toList()[index].data()['date'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Disease '+data.docs.toList()[index].data()['disease']['name'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Status '+data.docs.toList()[index].data()['disease']['status'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Op Number '+data.docs.toList()[index].data()['opnumber'],style: TextStyle(fontSize:20,color: Colors.white),),
+             new Text('Phone '+data.docs.toList()[index].data()['phone'].toString(),style: TextStyle(fontSize:20,color: Colors.white),),
+           ],
+         ),
+       )),
+     );
+    }
+  )
     );
+    }
+    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fund_folio/doctype/home.dart';
 
 //The main function is the starting point for all Flutter apps.
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
+    
     MaterialApp(
       theme: ThemeData(
         primaryColor: Colors.deepPurple,
